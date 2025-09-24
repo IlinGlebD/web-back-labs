5600d1f681306abc8aba75d976b4171585523447
Ильин Глеб <ilingleb1608@gmail.com>
2025-09-24 17:48:27 +0700

    Имя студента

--- app.py
+++ app.py
@@ -401,4 +401,6 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Ильин Глеб'
+    return render_template('example.html', name=name)
+    

d8266e270d240a959fd8e02b01bda10723e6a18b
Ильин Глеб <ilingleb1608@gmail.com>
2025-11-17 12:43:15 +0700

    Исправление SQL-инъекции

--- lab5.py
+++ lab5.py
@@ -39,13 +39,14 @@ def register():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html',
                                error='Такой пользователь уже существует')
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);",
+                (login, password_hash, ))
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
@@ -60,7 +61,7 @@ def login():
         return render_template('lab5/login.html', error='Заполните имя')
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user = cur.fetchone()
 
     if not user:
@@ -111,10 +112,10 @@ def list():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    cur.execute("SELECT id FROM users WHERE login=%s;", (login, ))
     login_id = cur.fetchone()["id"]
 
-    cur.execute(f"SELECT * FROM articles WHERE user_id='{login_id}';")
+    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (login_id, ))
     articles = cur.fetchall()
 
     db_close(conn, cur)

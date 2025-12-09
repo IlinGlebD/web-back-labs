aa0a77e2642467ac3d5f9020e05f73aa84aa270e
Ильин Глеб <ilingleb1608@gmail.com>
2025-12-09 10:11:54 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, url_for, request
 import datetime
 import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -23,6 +27,22 @@ app.register_blueprint(lab8)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',
                                           'секретно-секретный ключ')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'gleb_ilin_orm'
+    db_user = 'gleb_ilin_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "gleb_ilin_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 
 @app.route('/')

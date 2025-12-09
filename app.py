efdbcbaaa8b9c1858a92894618d84197d5861e07
Ильин Глеб <ilingleb1608@gmail.com>
2025-12-09 09:37:16 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
@@ -17,6 +18,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',
                                           'секретно-секретный ключ')
@@ -44,6 +46,7 @@ def index():
                     <li><a href="/lab5">Лабораторная работа 5</a></li>
                     <li><a href="/lab6">Лабораторная работа 6</a></li>
                     <li><a href="/lab7">Лабораторная работа 7</a></li>
+                    <li><a href="/lab8">Лабораторная работа 8</a></li>
                 </ul>
                 <hr>
                 <footer>

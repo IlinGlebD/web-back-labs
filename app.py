61c831e219558e2ddbedbb1a67513c8466615f24
Ильин Глеб <ilingleb1608@gmail.com>
2025-12-17 11:11:39 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask(__name__)
 
@@ -22,9 +23,11 @@ login_manager = LoginManager()
 login_manager.login_view = 'lab8.login'
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_users(login_id):
     return users.query.get(int(login_id))
+
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -34,6 +37,8 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
+
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',
                                           'секретно-секретный ключ')
@@ -78,6 +83,7 @@ def index():
                     <li><a href="/lab6">Лабораторная работа 6</a></li>
                     <li><a href="/lab7">Лабораторная работа 7</a></li>
                     <li><a href="/lab8">Лабораторная работа 8</a></li>
+                    <li><a href="/lab9">Лабораторная работа 9</a></li>
                 </ul>
                 <hr>
                 <footer>
